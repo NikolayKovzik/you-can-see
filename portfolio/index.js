8e952423ce45ae260dcf813bff5255f347d0214a
Nikolay <nikolay.kovzik@gmail.com>
2022-01-31 21:54:48 +0300

    portfolio 3

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,36 +1,99 @@
+import i18Obj from './translate.js';
 
-
-
+/*Hamburger menu*/
 
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-list');
 const menuItem = document.querySelectorAll('.nav-item');
+const blackout = document.querySelector('.blackout');
 
 function toggleMenu() {
-        hamburger.classList.toggle('open-menu');
-        menu.classList.toggle('nav-list-active');
+    hamburger.classList.toggle('open-menu');
+    menu.classList.toggle('nav-list-active');
+    blackout.classList.toggle('active');
 }
 
 hamburger.addEventListener('click', toggleMenu);
 
 
 menuItem.forEach(item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.toggle('open-menu');
-        menu.classList.toggle('nav-list-active');
-    })
+    item.addEventListener('click', toggleMenu)
 })
 
-console.log(`Вёрстка соответствует макету. Вёрстка соответствует макету. Ширина экрана 768px +48
-Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки.
-Весь контент страницы при этом сохраняется: не обрезается и не удаляется. Фоновые изображения,
-добавляемые свойством background-image, к контенту не относятся, их можно обрезать +15
-На ширине экрана 768рх и меньше реализовано адаптивное меню
 
-(При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана -
-Реализовано частично, так как не смог полноценно проверить этот пункт на pixel perfect при разрешениях от 768 до 620: 
-При включении расширения с открытым меню оно смещается относительно своего положения.
-Реализовал через относительные величины) 
 
-+20 
-Итог: 83`);
+
+/*Switching images*/
+
+const portfolioButtons = document.querySelectorAll('.portfolio-button');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+/*
+  const buttonWrapper = document.querySelector('.button-wrapper');
+  function switchImages(event) {
+    if(event.target.classList.contains('portfolio-button')) {
+      
+    }
+  }
+  buttonWrapper.addEventListener('click', switchImages);*/
+
+
+
+portfolioButtons.forEach((currentButton, seasonIndex) => {
+    currentButton.addEventListener('click', () => {
+        portfolioImages.forEach((img, imgIndex) => {
+            img.src = `./img/${seasons[seasonIndex]}/${imgIndex + 1}.jpg`;
+            img.alt = `${seasons[seasonIndex]} image`;
+        });
+        portfolioButtons.forEach((button,index) => {
+            seasonIndex == index ? (currentButton.classList.remove('button-dark'), currentButton.classList.add('button-gold'))
+                                 : button.classList.contains('button-gold') ? (button.classList.remove('button-gold') , button.classList.add('button-dark'))
+                                 : null;
+        });
+    });
+});
+
+
+/* Image caching*/
+
+
+function preloadImages() {
+    for(let season of seasons) {
+        for(let i = 1; i <= 6; i++) {
+            const img = new Image();
+            img.src = `./img/${seasons[season]}/${i}.jpg`;
+        }
+    }
+}
+
+preloadImages();
+
+
+/* Translate page*/
+
+
+const ruButton = document.querySelector('.ru');
+const engButton = document.querySelector('.en');
+
+function translatePage(event){
+    let data = document.querySelectorAll('[data-translate]');
+    for(let currentElement of data){
+        if (currentElement.placeholder) {
+            currentElement.placeholder = i18Obj[event.target.textContent][currentElement.getAttribute('data-translate')];
+            currentElement.textContent = '';
+          }
+        currentElement.textContent = i18Obj[event.target.textContent][currentElement.getAttribute('data-translate')];
+    }
+
+    (event.target.textContent === 'ru') ? document.querySelector('.nav-list').classList.add('ru-mode')
+                                        : document.querySelector('.nav-list').classList.remove('ru-mode');
+
+}
+
+ruButton.addEventListener('click',translatePage);
+engButton.addEventListener('click',translatePage);
+
+
+
+
