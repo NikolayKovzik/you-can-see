4baa3413a7578da872e3f5369d8c79ab8b752474
Nikolay <nikolay.kovzik@gmail.com>
2022-02-17 00:04:02 +0300

    trualalalal

---
+++ app.js
@@ -0,0 +1,147 @@
+//const TMDB_API_KEY = 'api_key=9012a3cfced9963877aeb95eb5fa8f78';
+const TMDB_API_KEY = 'api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=spring';
+const TMDB_URL = 'https://api.themoviedb.org/3';
+const POP_URL = `${TMDB_URL}/discover/movie?sort_by=popularity.desc&${TMDB_API_KEY}`
+const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500/';
+const SEARCH_URL = `${TMDB_URL}/search/movie?${TMDB_API_KEY}`;
+const movieWrapper = document.querySelector('.movie-wrapper');
+const searchForm = document.getElementById('form');
+const searchInput = document.querySelector('.search-input');
+const searchCross = document.querySelector('.cross-wrapper');
+const noResults = document.querySelector('.no-results');
+let numberPage = 1
+let isSearch = null;
+let searchInputValue = '';
+
+searchCross.addEventListener('click',() =>{
+     searchInput.value = '';
+});
+
+
+function getGenres(arr) {
+    let res = arr.reduce(function(res,item){
+        switch (item) {
+            case 28: return res + "Action/";
+            case 12: return res + "Adventure/";
+            case 16: return res + "Animation/";
+            case 35: return res + "Comedy/";
+            case 80: return res + "Crime/";
+            case 99: return res + "Documentary/";
+            case 18: return res + "Drama/";
+            case 10751: return res + "Family/";
+            case 14: return res + "Fantasy/";
+            case 36: return res + "History/";
+            case 27: return res + "Horror/";
+            case 10402: return res + "Music/";
+            case 9648: return res + "Mystery/";
+            case 10749: return res + "Romance/";
+            case 878: return res + "Science Fiction/";
+            case 10770: return res + "TV Movie/";
+            case 53: return res + "Thriller/";
+            case 10752: return res + "War/";
+            case 37: return res + "Western/";
+            default: return res;
+          }
+    },'');
+    return (res) ? res.slice(0,res.length-1) : '';
+}
+
+
+
+function showMovies(data) {
+    data.forEach(movie => {
+        const newMovie = document.createElement('div');
+        newMovie.classList.add('movie');
+        newMovie.innerHTML = `
+        <img class="movie-img" src=${(movie.poster_path) ? `${IMAGE_BASE}${movie.poster_path}` : './img/no-poster-found.jpg'}>
+        <h2 class="movie-title">${movie.title}</h2>
+        <div class="movie-category">${getGenres(movie.genre_ids)}</div>
+        <div class="movie-description">
+            <h3>Plot</h3>
+            ${(movie.overview) ? movie.overview : 'There is no description.'}
+        </div>
+        <div class="movie-mark ${(movie.vote_average>7) ? 'green' : (movie.vote_average>5) ? 'yellow' : 'red'}">${(movie.vote_average>0 && movie.vote_average<=10) ? movie.vote_average : 'N/A'}</div>
+         <div class="dark"></div> 
+        `;
+        movieWrapper.appendChild(newMovie);
+        newMovie.addEventListener('click',function(){// this.classList.toggle('rotate');
+                                                    this.querySelector('.movie-description').classList.toggle('rotate');});
+        newMovie.addEventListener('mouseover', function(){this.querySelector('.dark').classList.add('visible');});
+        newMovie.addEventListener('mouseout', function(){this.querySelector('.dark').classList.remove('visible');});
+    });
+}
+
+async function getMovies(url) {
+    noResults.classList.remove('visible');
+    let res = null;
+    let data = null;
+    let i = 0;
+    do{
+        res = await fetch(`${url}&page=${numberPage}`);
+        data = await res.json();
+       // console.log(res);
+        console.log(data);
+        console.log(data.page);
+        console.log(data.total_pages);
+        await showMovies(data.results);
+        (++i<3) ? numberPage++ : numberPage;
+    }
+    while(i<3 && (data.page < data.total_pages))
+
+    if(movieWrapper.querySelectorAll('.movie').length) {
+        (data.page < data.total_pages) ? infiniteObserver.observe(movieWrapper.querySelector('.movie:last-child'))
+                                       : (infiniteObserver.unobserve(movieWrapper.querySelector('.movie:last-child')));
+    }
+    else {
+        noResults.classList.add('visible');
+    }
+
+}
+
+
+function getNewCard() {
+    numberPage++
+	if (isSearch) {
+        if(searchInputValue)
+		    setTimeout(() => getMovies(`${SEARCH_URL}&query=${searchInputValue}`), 2000);
+        else
+            numberPage--;
+	} else {
+		setTimeout(() => getMovies(POP_URL), 2000);
+	}
+}
+
+ const infiniteObserver = new IntersectionObserver(([entery], observer) => {
+		if (entery.isIntersecting) {
+			observer.unobserve(entery.target);
+			getNewCard();
+		}
+	},  {threshold: 1});
+
+
+
+
+
+/*search*/
+
+searchForm.onsubmit = async (event) => {
+    event.preventDefault();
+    if(searchInput.value){
+        searchInputValue = searchInput.value;
+        movieWrapper.querySelectorAll('.movie').forEach((item)=>item.remove());
+        numberPage = 1;
+        isSearch = true;
+        getMovies(`${SEARCH_URL}&query=${searchInputValue}`);
+    }
+    
+  };
+
+
+
+// const previousPage = document.querySelector('.previous-page');
+// const nextPage = document.querySelector('.next-page');
+// const currentPage = document.querySelector('.current-page');
+
+
+
+getMovies(POP_URL);
